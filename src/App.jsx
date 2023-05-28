a6d10dd10806ad73d8df4f21a49931b5ee5bfc6e
LOVKUSH <lovkushyadav9888@gmail.com>
2023-05-28 15:22:52 +0530

    Adding Bootstrap in the react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//For calling the API we use the useEffect & useState because I want my data on load of my page and
+import { useState, useEffect } from "react";
 
 
 function App() {
 
+  const[products, getProducts] = useState([]);
+
+  useEffect (() => {
+
+    //Api fetching
+
+    fetch ('https://fakestoreapi.com/products')
+    .then(data => data.json())
+    .then(result => getProducts(result))
+
+  }, []);
+
   return (
     <>
       <h1>Products</h1>
+      
     </>
   )
 }
