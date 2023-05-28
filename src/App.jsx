6caba56ce8cef7c9c7b3a704167a2efc6e8c604c
LOVKUSH <lovkushyadav9888@gmail.com>
2023-05-28 15:31:55 +0530

    using mapping to display data on screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 //For calling the API we use the useEffect & useState because I want my data on load of my page and
 import { useState, useEffect } from "react";
 
@@ -18,9 +21,26 @@ function App() {
 
   }, []);
 
+
+  const cards = products.map(product => (
+    <div className="col-md-3" key={product.id}>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={product.image} />
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>{product.description}</Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  ));
+
   return (
     <>
       <h1>Products</h1>
+      <div className="row">
+      {cards}
+      </div>
       
     </>
   )
