c4dcda66c28ed0f9ee4d37c006ad0fe2779603cd
LOVKUSH <lovkushyadav9888@gmail.com>
2023-05-28 22:31:38 +0530

    adding the home and resposiveness

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import NavB from './assets/components/NavB';
+import Home from './assets/components/Home';
 import Products from './assets/components/Products';
 import Error from './assets/components/Error';
 import Cart from './assets/components/Cart';
@@ -13,7 +14,8 @@ function App() {
     <BrowserRouter>
     <NavB/>
       <Routes>
-        <Route path="/" element={<Products />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Products />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="*" element={<Error />} />
---
+++ src/assets/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Ecom. Store</h1>
+      <p>
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Tenetur ea aliquid pariatur. Molestias provident qui,
+        blanditiis repellendus ratione, rerum molestiae voluptatem culpa iusto
+        quis vero!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
