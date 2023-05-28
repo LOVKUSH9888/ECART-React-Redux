14c0df08ec81b974faf9cde211383e1ed51cbf08
LOVKUSH <lovkushyadav9888@gmail.com>
2023-05-28 23:34:17 +0530

    done with the store

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import {configureStore} form '@reduxjs/toolkit'
+import cartSlice from "./cartSlice"
+
+const store = configureStore({
+    reducer : {
+        cart : cartSlice,
+    }
+});
+
+export default store;
