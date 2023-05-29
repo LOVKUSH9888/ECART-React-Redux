257988e4fcb5e6081ec007cc04b187d50887c143
LOVKUSH <lovkushyadav9888@gmail.com>
2023-05-30 00:01:51 +0530

    added the Home a good Look

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -4,13 +4,41 @@ import { Link, NavLink } from "react-router-dom";
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Ecom. Store</h1>
-      <p>
-        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Tenetur ea aliquid pariatur. Molestias provident qui,
-        blanditiis repellendus ratione, rerum molestiae voluptatem culpa iusto
-        quis vero!
-      </p>
+    <div className="text-center">
+    <h1>Welcome to the Ecom. Store</h1>
+    </div>
+      
+      <div className="container content">
+        <p>
+          At our store, we strive to offer a wide range of high-quality products
+          to cater to your needs. Whether you're looking for fashion,
+          electronics, home decor, or more, we have something for everyone.
+          Customer service is at the heart of everything we do. If you have any
+          questions, concerns, or feedback, our friendly and knowledgeable
+          support team is here to assist you. We want to make sure your shopping
+          experience is seamless and enjoyable from start to finish.
+          At our store, we strive to offer a wide range of high-quality products
+          to cater to your needs.
+        </p>
+        <p>
+          Our team is dedicated to ensuring your satisfaction. We carefully
+          curate our products to ensure they meet our quality standards, and we
+          constantly update our inventory to bring you the latest trends and
+          innovations.Thank you for choosing our e-commerce store. We appreciate
+          your trust and look forward to serving you. Happy shopping!
+          Our team is dedicated to ensuring your satisfaction. We carefully
+          curate our products to ensure they meet our quality standards, and we
+          constantly update our inventory to bring you the latest trends and
+          innovations.Thank you for choosing our e-commerce store. We appreciate
+          your trust and look forward to serving you. Happy shopping!
+          Our team is dedicated to ensuring your satisfaction. We carefully
+          curate our products to ensure they meet our quality standards, and we
+          constantly update our inventory to bring you the latest trends and
+          innovations.Thank you for choosing our e-commerce store. We appreciate
+          your trust and look forward to serving you. Happy shopping!
+          Our team is dedicated to ensuring your satisfaction.
+        </p>
+      </div>
     </div>
   );
 };
