171f8873d658f92d30822ee40a5caa9dc30ede3e
LOVKUSH <99957868+LOVKUSH9888@users.noreply.github.com>
2024-05-02 05:22:57 +0530

    Update cartSlice.js
    updated : bug resolved

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -11,7 +11,7 @@ const cartSlice = createSlice({
         },
 
         remove(state, action) {
-            return state.filter(item => item.id !== action.payload)
+            return state.filter(item => item.id !== action.payload.id)
         }
     }
 });
